1ba4ff9ce4f1a73c0c5547a60ec193c056f527e9
Arianna <44093846+starry-ari@users.noreply.github.com>
2025-09-02 13:41:13 -0400

    test2

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-tmux kill-server
-python3 -m venv venv
 
-cd /root/AriannaRPortfolio
+
+
+cd /root/ARportfolio2
 
 git fetch && git reset origin/main --hard
 
 
-source venv/bin/activate
 
-
-
-
-tmux new-session -d -s flask_server
-
-source python3-virtualvenv/bin/activate 
- docker compose -f docker-compose.prod.yml down
-  docker compose -f docker-compose.prod.yml up -d --build
-python3 run.py
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
